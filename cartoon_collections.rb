2e3178d70b14ddda0cd431b4dda97a3ebaa15694
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 02:01:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,11 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  array.any? do |item|
+    item.length >4
 end
 
 def find_the_cheese# code an argument here
